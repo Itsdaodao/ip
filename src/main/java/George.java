0ce554730859fdd029b4e43dbd910166af05fec8
daohang <wangdaohang123@gmail.com>
2025-08-21 17:08:04 +0800

    added monkey emoji and customised new line

--- src/main/java/George.java
+++ src/main/java/George.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 public class George {
     public static void main(String[] args) {
         String BANANA = "\uD83C\uDF4C";
+        String MONKEY = "🐒";
 
-        String greeting = "Ooo eee ooo aaa I am George the monkey";
-        String exit = "I love bananas " + BANANA.repeat(5) + " please bring banana next time";
-        System.out.println(greeting + "\n" + exit);
+        String newLine = MONKEY + BANANA.repeat(20) + MONKEY;
+        Scanner scanner = new Scanner(System.in);
+
+
+        String greeting = "Ooo eee ooo aaa aaa I am George the Monkey"
+                + MONKEY
+                + "\nGeorge help you with?";
+        System.out.println(greeting);
+        System.out.println(newLine);
+
+        String exit = "I love bananas " + BANANA.repeat(5) + "\nplease bring banana next time";
+        System.out.println("\n" + exit);
     }
 }
