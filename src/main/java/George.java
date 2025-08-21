bd8b428baab97b470bf51fbb0adf6edb149065fc
daohang <wangdaohang123@gmail.com>
2025-08-21 22:57:25 +0800

    added echo feature

--- src/main/java/George.java
+++ src/main/java/George.java
@@ -5,17 +5,24 @@ public class George {
         String BANANA = "\uD83C\uDF4C";
         String MONKEY = "🐒";
 
-        String newLine = MONKEY + BANANA.repeat(20) + MONKEY;
+        String newLine = "\n" + MONKEY + BANANA.repeat(20) + MONKEY + "\n";
         Scanner scanner = new Scanner(System.in);
-
-
         String greeting = "Ooo eee ooo aaa aaa I am George the Monkey"
                 + MONKEY
                 + "\nGeorge help you with?";
         System.out.println(greeting);
         System.out.println(newLine);
 
-        String exit = "I love bananas " + BANANA.repeat(5) + "\nplease bring banana next time";
-        System.out.println("\n" + exit);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                String exit = "I love bananas " + BANANA.repeat(5)
+                        + "\nplease bring bananas next time";
+                System.out.println("\n" + exit);
+                break;
+            }
+
+            System.out.println(newLine + input + newLine);
+        }
     }
 }
