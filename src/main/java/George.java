c9cb1b03e023239952cac190c5ff058751bd6f0c
daohang <wangdaohang123@gmail.com>
2025-08-21 16:48:34 +0800

    Renamed chatbot and added greeting + exit

---
+++ src/main/java/George.java
@@ -0,0 +1,9 @@
+public class George {
+    public static void main(String[] args) {
+        String BANANA = "\uD83C\uDF4C";
+
+        String greeting = "Ooo eee ooo aaa I am George the monkey";
+        String exit = "I love bananas " + BANANA.repeat(5) + " please bring banana next time";
+        System.out.println(greeting + "\n" + exit);
+    }
+}
