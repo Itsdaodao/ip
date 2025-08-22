0d15f51871a003ae633247287647419fb5e779db
daohang <wangdaohang123@gmail.com>
2025-08-22 09:00:48 +0800

    changed task to an interface

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,30 +1,7 @@
-public class Task {
-    private String description;
-    private boolean isDone;
-
-    public Task(String description) {
-        this.description = description;
-        this.isDone = false;
-    }
-
-    public String getDescription() {
-        return this.description;
-    }
-
-    public boolean isDone() {
-        return isDone;
-    }
-
-    public void markAsDone() {
-        this.isDone = true;
-    }
-
-    public void markAsNotDone() {
-        this.isDone = false;
-    }
-
-    @Override
-    public String toString() {
-        return this.description;
-    }
+interface Task {
+    String getDescription();
+    boolean isDone();
+    void markAsDone();
+    void markAsNotDone();
+    String getStatus();
 }
