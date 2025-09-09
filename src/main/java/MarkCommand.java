a47a6ab575a677947eb44bed3101174cd559313d
daohang <wangdaohang123@gmail.com>
2025-09-09 09:55:23 +0800

    add mark and unmark command class

---
+++ src/main/java/MarkCommand.java
@@ -0,0 +1,19 @@
+import exceptions.GeorgeException;
+
+public class MarkCommand extends Command {
+    private final int taskNumber;
+
+    public MarkCommand(int taskNumber) {
+        this.taskNumber = taskNumber;
+    }
+
+    @Override
+    public void execute(TaskManager manager) throws GeorgeException {
+        manager.markTaskAsDone(taskNumber);
+    }
+
+    @Override
+    public String getCommandWord() {
+        return "Mark";
+    }
+}
---
+++ src/main/java/UnmarkCommand.java
@@ -0,0 +1,19 @@
+import exceptions.GeorgeException;
+
+public class UnmarkCommand extends Command {
+    private final int taskNumber;
+
+    public UnmarkCommand(int taskNumber) {
+        this.taskNumber = taskNumber;
+    }
+
+    @Override
+    public void execute(TaskManager manager) throws GeorgeException {
+        manager.markTaskAsNotDone(taskNumber);
+    }
+
+    @Override
+    public String getCommandWord() {
+        return "Unmark";
+    }
+}
