6d4aa2727925fef7b6f2fb59a0babf84992dbf91
daohang <wangdaohang123@gmail.com>
2025-09-09 10:49:03 +0800

    refactor George.java

--- src/main/java/George.java
+++ src/main/java/George.java
@@ -42,42 +42,18 @@ public class George {
         while (scanner.hasNextLine()) {
             String input = scanner.nextLine();
             try {
-                if (input.equals("bye")) {
-                    String exit = "I love bananas " +
-                            "\nplease bring bananas next time";
-                    System.out.println("\n" + exit);
+                Command command = CommandParser.parse(input);
+                command.execute(manager);
+
+                if (command instanceof ExitCommand) {
                     break;
-                } else if (input.equals("list")) {
-                    manager.listTasks();
-                } else if (input.startsWith("mark ")) {
-                    int taskNumber = Integer.parseInt(input.substring(5));
-                    manager.markTaskAsDone(taskNumber);
-                } else if (input.startsWith("unmark ")) {
-                    int taskNumber = Integer.parseInt(input.substring(7));
-                    manager.markTaskAsNotDone(taskNumber);
-                } else if (input.startsWith("todo ")) {
-                    String description = input.substring(5);
-                    manager.addToDoTask(description);
-                } else if (input.startsWith("deadline ")) {
-                    String content = input.substring(9);
-                    String description = content.split("/by")[0].trim();
-                    String date = content.split("/by")[1].trim();
-                    manager.addDeadlineTask(description, date);
-                } else if (input.startsWith("event ")) {
-                    String content = input.substring(6);
-                    String[] splits = content.split("/from | /to");
-                    String description = splits[0].trim();
-                    String start = splits[1].trim();
-                    String end = splits[2].trim();
-                    manager.addEventTask(description, start, end);
-                } else if (input.startsWith("delete ")) {
-                    int taskNumber = Integer.parseInt(input.substring(7));
-                    manager.deleteTask(taskNumber);
-                } else {
-                    throw new GeorgeException("What are you saying???");
                 }
-            }  catch (GeorgeException e) {
+            } catch (GeorgeException e) {
                 System.out.println(e.toString());
+            } catch (NumberFormatException e) {
+                System.out.println("Please provide a valid task number!");
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
             }
         }
         scanner.close();
